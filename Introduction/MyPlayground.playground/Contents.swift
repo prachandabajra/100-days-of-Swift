7e5e84d114a1529cddfec4736561581618f9604b
Prachanda Muni Bajracharya <prachanda@Prachandas-MacBook-Pro.local>
2021-01-13 05:15:33 +0545

    Day 2, learned complex data types: arrays, dictionaries, sets and enums

--- Introduction/MyPlayground.playground/Contents.swift
+++ Introduction/MyPlayground.playground/Contents.swift
@@ -42,3 +42,79 @@ let height: Double = 1.78
 let taylorRocks: Bool = true
 var str5: String
 str5 = "Prachanda"
+
+// Complex Types
+// Arrays
+// Note: If you're using type annotations, arrays are written in brackets: [String], [Int], [Double], [Bool].
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+beatles[1]
+
+// Sets
+let colors = Set(["red", "green", "blue"])
+let colors2 = Set(["red", "green", "blue", "red", "blue"])
+
+// Tuples
+var name = (first: "Taylor", last: "Swift")
+name.0
+name.first
+
+// Arrays vs sets vs tuples
+// Tuples: fixed collection of related values where each item has a precise position or name
+// Sets: a collection of values that must be unique or you need to be able to check whether a specific item is in there extremely quickly.
+// Arrays: a collection of values that can contain duplicates, or the order of your items matters.
+let address = (house: 555, street: "Taylor Swift Avenue", city: "Nashville")
+let set = Set(["aardvark", "astronaut", "azalea"])
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+
+// Dictionaries
+// Note: When using type annotations, dictionaries are written in brackets with a colon between your identifier and value types. For example, [String: Double] and [String: String].
+let heights = ["Taylor Swift": 1.78, "Ed Sheeran": 1.73]
+heights["Taylor Swift"]
+
+// Dictionary default values
+let favouriteIceCream = ["Paul": "Chocolate", "Sophie": "Vanilla"]
+favouriteIceCream["Paul"]
+favouriteIceCream["Charlotte", default: "Unknown"]
+
+// Creating empty collections
+// Arrays, sets and dictionaries are called collections, because they collect values together in one place
+var teams = [String: String]()
+teams["Paul"] = "Red"
+
+var results2 = [Int]()
+
+var words = Set<String>()
+var numbers = Set<Int>()
+
+var scores = Dictionary<String, Int>()
+var results3 = Array<Int>()
+
+// Enumerations
+enum Result {
+    case success
+    case failure
+}
+let result4 = Result.failure
+
+// Enum associated values
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+let talking = Activity.talking(topic: "football")
+
+// Enum raw values
+enum Planet: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+let earth = Planet(rawValue: 2)
