435d216c89ffa869230f7e282660b269f5652b9a
Prachanda Muni Bajracharya <prachanda@Prachandas-MacBook-Pro.local>
2021-01-12 05:50:07 +0545

    Day 1, learned simple types: varables, strings, ...

---
+++ Introduction/MyPlayground.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+// Simple Types
+// Variables
+var str = "Hello, playground"
+str = "Prachanda"
+
+// Strings and integers
+var age = 38
+var population = 8_000_000
+
+// Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and Booleans
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+var score = 85
+var str3 = "Your score war \(score)"
+var results = "The test results are here: \(str3)"
+
+// Constants
+let taylor = "swift"
+
+// Type annotations
+// type inference: Swift is able to infer the type of something based on how you created it.
+let str4 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+var str5: String
+str5 = "Prachanda"
