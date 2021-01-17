5a2433d2a78615e0d662676b9aa72a6f91ff8536
Prachanda <prachanda.mb@gmail.com>
2021-01-17 15:21:06 +0545

    Day 6, learned closures part one

--- Introduction/Introduction2.playground/Contents.swift
+++ Introduction/Introduction2.playground/Contents.swift
@@ -117,3 +117,38 @@ print(user.first)
     // code here
 // }
 // it’s best to avoid creating inout parameters until you feel confident they are definitely the right choice.
+
+// Closures ------------------------------------------------------------------------------------------------------------------------------------------------
+// Creating basic closures
+let driving = {
+    print("I'm driving in my car")
+}
+driving()
+
+// Accepting parameters in closures
+// - One of the differences between functions and closures is that you don’t use parameter labels when running closures.
+// Returning values from a closure
+let driving2 = { (place: String) -> String in
+    return "I'm going to \(place) in my car."
+}
+let message = driving2("London")
+print(message)
+
+// Closures as parameters
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel(action: driving)
+
+// Trailing closure syntax
+// If the last parameter to a function is a closure, Swift lets you use special syntax called trailing closure syntax. Rather than pass in your closure as a parameter, you pass it directly after the function inside braces.
+travel() {
+    print("Trailing closure")
+}
+// because there aren’t any other parameters, we can eliminate the parentheses entirely
+travel {
+    print("Trailing closure2")
+}
+
