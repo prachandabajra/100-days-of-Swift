9fa4c1494a92a3aa258f1995df5a6f90b5d66b43
Prachanda <prachanda.mb@gmail.com>
2021-01-18 10:09:03 +0545

    Day 7, finished learning closures

--- Introduction/Introduction2.playground/Contents.swift
+++ Introduction/Introduction2.playground/Contents.swift
@@ -152,3 +152,108 @@ travel {
     print("Trailing closure2")
 }
 
+// Using closures as parameters when they accept parameters
+func travel2(action: (String) -> Void) {
+    print("I'm getting ready to go.")
+    action("London")
+    print("I arrived!")
+}
+travel2 { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+// Using closures as parameters when they return values
+func travel3(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+travel3 { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+// Shorthand parameter names
+// - Swift knows the parameter to that closure must be a string, so we can remove it
+travel3 { place -> String in
+    return "I'm going to \(place) in my car"
+}
+// It also knows the closure must return a string, so we can remove that:
+travel3 { place in
+    return "I'm going to \(place) in my car"
+}
+// As the closure only has one line of code that must be the one that returns the value, so Swift lets us remove the return keyword too:
+travel3 { place in
+    "I'm going to \(place) in my car"
+}
+// Rather than writing place in we can let Swift provide automatic names for the closure’s parameters. These are named with a dollar sign, then a number counting from 0.
+travel3 {
+    "I'm going to \($0) in my car"
+}
+
+// Closures with multiple parameters
+func travel4(action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 60)
+    print(description)
+    print("I arrived!")
+}
+travel4 {
+    "I'm going to \($0) at \($1) miles per hour."
+}
+
+// Returning closures from functions
+func travel5() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+let result2 = travel5()
+result2("London")
+// It’s technically allowable – although really not recommended! – to call the return value from travel() directly:
+travel5()("London")
+
+// Capturing values
+// If you use any external values inside your closure, Swift captures them – stores them alongside the closure, so they can be modified even if they don’t exist any more.
+func travel6() -> (String) -> Void {
+    var counter = 1
+
+    return {
+        print("\(counter). I'm going to \($0)")
+        counter += 1
+    }
+}
+let result3 = travel6()
+result3("London")
+result3("London")
+result3("London")
+result3("London")
+
+// When would you use closures with return values as parameters to a function?
+func reduce(_ values: [Int], using closure: (Int, Int) -> Int) -> Int {
+    // start with a total equal to the first value
+    var current = values[0]
+
+    // loop over all the values in the array, counting from index 1 onwards
+    for value in values[1...] {
+        // call our closure with the current value and the array element, assigning its result to our current value
+        current = closure(current, value)
+    }
+
+    // send back the final current value
+    return current
+}
+
+// - With that code in place, we can now write this so add up an array of numbers:
+let numbers = [10, 20, 30]
+
+let sum = reduce(numbers) { (runningTotal: Int, next: Int) in
+    runningTotal + next
+}
+
+print(sum)
+
+// - I mentioned previously that Swift’s operators are actually functions in their own right. For example, + is a function that accepts two numbers (e.g. 5 and 10) and returns another number (15). As a result, we can actually write this:
+let sum2 = reduce(numbers, using: +)
+// Third, this reduce() function is so important that a variant actually comes with Swift as standard. The concept is the same, but it’s more advanced in several ways
+
