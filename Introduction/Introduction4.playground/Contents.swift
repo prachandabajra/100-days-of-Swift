81e59cad663fd10cd611cc1c64a87b69a4d3a1b7
Prachanda <prachanda.mb@gmail.com>
2021-01-27 14:57:04 +0545

    Day 15, reviewed properties, static properties/methods, access control, polymorphism and typecasting, closures'

--- Introduction/Introduction4.playground/Contents.swift
+++ Introduction/Introduction4.playground/Contents.swift
@@ -122,3 +122,118 @@ getHaterStatus4(weather: WeatherType4.wind(speed: 5))
 //Working with Objective-C code
 //If you want to have some part of Apple’s operating system call your Swift class’s method, you need to mark it with a special attribute: @objc. This is short for “Objective-C”, and the attribute effectively marks the method as being available for older Objective-C code to run – which is almost all of iOS, macOS, watchOS, and tvOS. For example, if you ask the system to call your method after one second has passed, you’ll need to mark it with @objc.
 //Don’t worry too much about @objc for now – not only will I be explaining it in context later on, but Xcode will always tell you when it’s needed. Alternatively, if you don’t want to use @objc for individual methods you can put @objcMembers before your class to automatically make all its methods available to Objective-C.
+
+// Swift Review 3
+// Properties
+// Property Observers
+struct Person {
+    var clothes: String {
+        willSet {
+            updateUI(msg: "I'm changing from \(clothes) to \(newValue)")
+        }
+
+        didSet {
+            updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
+        }
+    }
+}
+
+func updateUI(msg: String) {
+    print(msg)
+}
+
+var taylor = Person(clothes: "T-shirts")
+taylor.clothes = "short skirts"
+
+// Computed properties
+// To make a computed property, place an open brace after your property then use either get or set to make an action happen at the appropriate time.
+struct Person2 {
+    var age: Int
+
+    var ageInDogYears: Int {
+        get {
+            return age * 7
+        }
+    }
+}
+
+var fan = Person2(age: 25)
+print(fan.ageInDogYears)
+
+// Note: If you intend to use them only for reading data you can just remove the get part entirely, like this:
+//var ageInDogYears: Int {
+//    return age * 7
+//}
+
+// Access control
+// Access control lets you specify what data inside structs and classes should be exposed to the outside world, and you get to choose four modifiers:
+// Public: this means everyone can read and write the property.
+// Internal: this means only your Swift code can read and write the property. If you ship your code as a framework for others to use, they won’t be able to read the property.
+// File Private: this means that only Swift code in the same file as the type can read and write the property.
+// Private: this is the most restrictive option, and means the property is available only inside methods that belong to the type, or its extensions.
+
+// Polymorphism and typecasting
+class Album {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func getPerformance() -> String {
+        return "The album \(name) sold lots"
+    }
+}
+
+class StudioAlbum: Album {
+    var studio: String
+
+    init(name: String, studio: String) {
+        self.studio = studio
+        super.init(name: name)
+    }
+
+    override func getPerformance() -> String {
+        return "The studio album \(name) sold lots"
+    }
+}
+
+class LiveAlbum: Album {
+    var location: String
+
+    init(name: String, location: String) {
+        self.location = location
+        super.init(name: name)
+    }
+
+    override func getPerformance() -> String {
+        return "The live album \(name) sold lots"
+    }
+}
+
+var taylorSwift = StudioAlbum(name: "Taylor Swift", studio: "The Castles Studios")
+var fearless = StudioAlbum(name: "Speak Now", studio: "Aimeeland Studio")
+var iTunesLive = LiveAlbum(name: "iTunes Live from SoHo", location: "New York")
+
+var allAlbums: [Album] = [taylorSwift, fearless, iTunesLive]
+
+// Typecasting in Swift comes in three forms, but most of the time you'll only meet two: as? and as!, known as optional downcasting and forced downcasting.
+for album in allAlbums {
+    print(album.getPerformance())
+    
+    if let studioAlbum = album as? StudioAlbum {
+        print(studioAlbum.studio)
+    } else if let liveAlbum = album as? LiveAlbum {
+        print(liveAlbum.location)
+    }
+}
+
+// Swift also allows optional downcasting at the array level, although it's a bit more tricksy because you need to use the nil coalescing operator to ensure there's always a value for the loop. Here's an example:
+for album in allAlbums as? [LiveAlbum] ?? [LiveAlbum]() {
+    print(album.location)
+}
+
+// Converting common types with initializers
+let number = 5
+let text = String(number)
+print(text)
