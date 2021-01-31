ccee0031a31511d1aa83ec1438a23a9680f7a45c
Prachanda <prachanda.mb@gmail.com>
2021-01-31 13:38:28 +0545

    Day 20, Project 2 learned array shuffling, random number generation, @IBAction and UIAlertController

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -15,16 +15,19 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
        
+        // CALayer
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
         
+        // CALayer doesn't understand UIColor, so it need to be converted to CGColor
         // for custom color
 //        button1.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0).cgColor
         button1.layer.borderColor = UIColor.lightGray.cgColor
@@ -34,12 +37,36 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
-        button2.setImage(UIImage(named: countries[2]), for: .normal)
-        button3.setImage(UIImage(named: countries[3]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
 
-
+    //  The event used for the attachment is called TouchUpInside
+    // @IBOutlet is a way of connecting code to storyboard layouts, and @IBAction is a way of making storyboard layouts trigger code.
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if (sender.tag == correctAnswer) {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
 }
 
