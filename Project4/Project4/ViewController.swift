e4877b97e5a5c75a9d62a86acec30e7f2364a1b4
Prachanda <prachanda.mb@gmail.com>
2021-02-04 13:24:11 +0545

    Day 24, Project 4 learned WKWebView and action sheets

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Prachanda Muni Bajracharya on 2/4/21.
+//  Copyright © 2021 Prachanda Muni Bajracharya. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+// ViewController inherits from UIViewController class and conforms to WKNavigationDelegate protocol.
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else {
+            return
+        }
+        guard let url = URL(string: "https://" + actionTitle) else {
+            return
+        }
+//        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+
+}
+
