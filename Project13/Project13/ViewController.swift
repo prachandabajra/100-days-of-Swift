e582e1bd9f7a98dd9399cb968f89d26253eccc0b
Prachanda <prachanda.mb@gmail.com>
2021-03-07 09:15:52 +0545

    Day 54, Project 13 complete

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -16,10 +16,15 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
+    @IBOutlet var radius: UISlider!
+    @IBOutlet var changeFilter: UIButton!
+    
     var currentImage: UIImage!
     
     var context: CIContext!
     var currentFilter: CIFilter!
+    
+    let defaultFilter = "CISepiaTone"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +33,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
         
         context = CIContext()
-        currentFilter = CIFilter(name: "CISepiaTone")
+        currentFilter = CIFilter(name: defaultFilter)
     }
     
     @objc func importPicture() {
@@ -51,6 +56,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         
         applyProcessing()
+        
+        if changeFilter.titleLabel?.text == "Change Filter" {
+            changeFilter.setTitle(defaultFilter, for: .normal)
+        }
     }
 
     // For iPad we need popoverPresentationController
@@ -81,7 +90,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     // This second option is longer, but provides much more information both to Xcode and to other people reading your code, so it's generally preferred. To be honest, this particular callback is a bit of a wart in iOS, but the fact that it stands out so much is testament to the fact that there are so few warts around!
     // #selector(image(_:didFinishSavingWithError:contextInfo:))
     @IBAction func save(_ sender: Any) {
-        guard let image = imageView.image else { return }
+        guard let image = imageView.image else {
+            let ac = UIAlertController(title: "Save Error", message: "There is no image to save", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            return
+        }
         
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
@@ -99,7 +113,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         // CIBumpDistortion uses inputCenter, inputRadius, inputScale
         // CIGaussianBlur uses inputRadius
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radius.value * 200, forKey: kCIInputRadiusKey) }
         if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
         if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
 
@@ -120,6 +134,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         
         // safely read the alert action's title
         guard let actionTitle = action.title else { return }
+        
+        changeFilter.setTitle(actionTitle, for: .normal)
 
         currentFilter = CIFilter(name: actionTitle)
         
